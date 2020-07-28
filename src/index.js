82050fe6e93c4aeb655dcd2a950d4ee957af92a6
Sergiu Mare <mare.sergiu@yahoo.com>
2020-07-28 19:29:41 +0300

    master:master

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './scss/main.scss';
+import './scss/libs/normalize.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/views/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="home-page">
+                <div className="holder">
+                    <h2 className="sub-title">Home page!</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
