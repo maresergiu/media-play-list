96105e55680d86415c8644b55214d62152fca575
Sergiu Mare <mare.sergiu@yahoo.com>
2020-07-29 14:13:02 +0300

    master: the api and the init call to fecth the data

--- src/App.js
+++ src/App.js
@@ -6,22 +6,28 @@ import {
   Redirect
 } from "react-router-dom";
 
+import { Provider } from "react-redux";
+
+import store from "./store/index";
+
 import HomePage from "./views/HomePage.jsx";
 import NoMatch from "./views/NoMatch.jsx";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Redirect exact from="/" to="/home" />
-        <Route path="/home">
-          <HomePage />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import GlobalComponents from "../components/GlobalComponents.jsx";
+import MediaList from "../components/MediaList.jsx";
 
 class HomePage extends Component {
     render() {
@@ -6,7 +8,9 @@ class HomePage extends Component {
             <div className="home-page">
                 <div className="holder">
                     <h2 className="sub-title">Home page!</h2>
+                    <MediaList />
                 </div>
+                <GlobalComponents />
             </div>
         );
     }
