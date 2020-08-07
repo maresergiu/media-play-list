a70772e536eb25cd1f718dbe85cb8214983c8f84
Sergiu Mare <mare.sergiu@yahoo.com>
2020-08-07 10:57:56 +0300

    master: change comments

--- src/tests/unit/App.test.js
+++ src/tests/unit/App.test.js
@@ -2,16 +2,8 @@ import React from "react"
 import { render } from "@testing-library/react"
 import App from "../../App"
 
-// due to the fact the I have written tests only for Vue.js apps until now
-// this application gived me the ooportunity to write tests also for React.js
-
-// this application brought a certain level of complexity as I had to learn the library very fast
-// and write unit tests with it
-
 // I have only written unit tests for the time being due to the short time that we had to spend writting the app
-// in a real production env I should write also integration tests and end-to-end tests
-
-// I hope you like my tests :)
+// In a real production env I should write also integration tests and end-to-end tests
 
 test("should contain the home-page", () => {
   const { queryByTestId } = render(<App />)
